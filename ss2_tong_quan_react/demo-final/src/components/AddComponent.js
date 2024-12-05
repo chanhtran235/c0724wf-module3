13cc677f40ee421ecdf764a11028a2cf6f151582
chanhtv235 <chanhtv235@gmail.com>
2024-12-05 19:50:48 +0700

    form

--- ss2_tong_quan_react/demo-final/src/components/AddComponent.js
+++ ss2_tong_quan_react/demo-final/src/components/AddComponent.js
@@ -1,31 +1,50 @@
-import React, {useRef} from "react";
+import React, {useRef, useState} from "react";
 import {addNewProduct, getAllProduct} from "../services/productService";
 import {useNavigate} from 'react-router-dom'
+import {Formik, Form, Field, ErrorMessage} from "formik";
+import * as Yup from 'yup';
+
+
 function AddComponent() {
-    // useRef
-    let idRef = useRef();
-    let nameRef = useRef();
+
+    const [product, setProduct] = useState({id: "", name: ""});
     // dùng để chuyến hướng ứng dụng
     const navigate = useNavigate();
-
-    const  handleAddNewProduct = ()=>{
-        const  product = {
-            id: idRef.current.value,
-            name: nameRef.current.value
-        }
-        // goi service
-        addNewProduct(product);
-        // chuyển hướng sang trang list
+    const handleSubmit = (value) => {
+        console.log(value);
+        addNewProduct(value);
         navigate('/products');
     }
+    const handleValidate =Yup.object({
+        id: Yup.number().required(" Id không được để trống")
+            .min(1,"Phải là số nguyên dương"),
+        name: Yup.string().required("Tên không được để trống")
+            .matches(/^[A-Z][a-z]*(\s[A-Z][a-z]*)+$/,'Tên chưa đúng định dạng')
+    })
+
+// validate: kiểm tra tính hợp lệ của dữ liệu trước khi lưu vào csdl
+
     return (
         <>
-            <form>
-                <input ref={idRef} placeholder={'Enter ID'} />
-                <input ref={nameRef} placeholder={'Enter Name'} />
-                <button type={'button'} onClick={handleAddNewProduct}>Save</button>
-            </form>
+            <Formik initialValues={product} onSubmit={handleSubmit} validationSchema={handleValidate}>
+                <Form>
+                    <div>
+                        <label>ID:</label>
+                        <Field type='text' name='id'/>
+                        <ErrorMessage name='id' style ={{color:'red'}} component ='div'/>
+                    </div>
+                    <div>
+                        <label>Name:</label>
+                        <Field type='text' name='name'/>
+                        <ErrorMessage name='name' style ={{color:'red'}} component ='div'/>
+                    </div>
+                    <div>
+                        <button type={'submit'}>Save</button>
+                    </div>
+                </Form>
+            </Formik>
         </>
     )
 }
-export default AddComponent ;
+
+export default AddComponent;
--- ss2_tong_quan_react/demo2/src/function_component/student/AddComponent.js
+++ ss2_tong_quan_react/demo2/src/function_component/student/AddComponent.js
@@ -1,42 +1,69 @@
-
 import React, {useRef, useState} from "react";
 import {addNew, getAll} from "../../service/studentService";
 import useForm from "../../hook/userForm";
 import {useNavigate} from "react-router-dom";
+import {Formik, Form, Field, ErrorMessage} from "formik";
+import * as Yup from "yup";
 
 function AddComponent() {
     const navigate = useNavigate()
-
-    const [formValues, handleOnChange, resetFrom] = useForm({id:"",name:""});
-    // const [student, setStudent] = useState({})
-    //
-    // const handleOnChange =(event)=>{
-    //     console.log(event.target.name);
-    //     console.log(event.target.value);
-    //     setStudent(prevState => ({
-    //         ...prevState,
-    //         [event.target.name]: event.target.value
-    //     }))
-    // }
-
-    const handleAdd =()=>{
-        console.log(formValues)
-        addNew(formValues);
-        resetFrom();
-        console.log(getAll());
-        navigate('/students')
+    const [student, setStudent] = useState({id: "", name: "",gender : "Female",subject: [], classId:"C04"})
+    const handleSubmit = (value) => {
+        console.log(value)
     }
-
+    const handleValidate = Yup.object({
+        id: Yup.string().required("yêu cầu nhập id"),
+        name: Yup.string().required("Yêu cầu nhập tên"),
+        gender: Yup.string().required("Yêu cầu nhập tên"),
+        subject:Yup.array().required("yêu cầu nhập").min(1,"Yêu cầu chọn ít nhất 1 môn"),
+        classId:Yup.string().required("Yêu cầu chọn")
+    })
     return (
         <>
-            {console.log("----add-render-----------")}
-            <form>
-                <input value={formValues.id}  onChange={handleOnChange} name={'id'} placeholder={'ID'}/>
-                <input value={formValues.name} onChange={handleOnChange} name={'name'}  placeholder={'Name'}/>
-                <button type={'button'} onClick={handleAdd}>Save</button>
-            </form>
+            <Formik initialValues={student} onSubmit={handleSubmit} validationSchema={handleValidate}>
+                <Form>
+                    <div>
+                        <label>ID</label>
+                        <Field type="text" name={'id'} placeholder={'ID'}/>
+                        <ErrorMessage name ='id' style ={{color:'red'}} component ='div' className ='error'/>
+                    </div>
+                    <div>
+                        <label>Name</label>
+                        <Field type="text" name={'name'} placeholder={'Name'}/>
+                        <ErrorMessage name ='name' style ={{color:'red'}} component ='div' className ='error'/>
+                    </div>
+                    <div>
+                        <label>Gender</label>
+                        <Field  type="radio" name={'gender'} value ={'Male'}/>Male
+                        <Field  type="radio" name={'gender'} value ={'Female'}/>Female
+                        <ErrorMessage name ='gender'style ={{color:'red'}}  component ='div' className ='error'/>
+                    </div>
+
+                    <div>
+                        <label>Subject</label>
+                        <Field  type="checkbox" name={'subject'} value ={'Java'}/>Java
+                        <Field  type="checkbox" name={'subject'} value ={'JS'}/> JS
+                        <Field  type="checkbox" name={'subject'} value ={'PHP'}/>PHP
+                        <ErrorMessage name ='subject' style ={{color:'red'}}  component ='div' className ='error'/>
+                    </div>
+                    <div>
+                        <label>Class</label>
+                        <Field  as="select" name={'classId'}>
+                          <option>--------Select--------</option>
+                          <option value={'C03'}>C03</option>
+                          <option value={'C04'}>C04</option>
+                          <option value={'C05'}>C05</option>
+                        </Field>
+                        <ErrorMessage name ='classId' style ={{color:'red'}} component ='div' className ='error'/>
+                    </div>
+                    <div>
+                        <button type={'submit'}>Save</button>
+                    </div>
+                </Form>
+            </Formik>
         </>
     )
 
 }
-export default AddComponent ;
+
+export default AddComponent;
